5dd898454e7cd2eb021cd9571acd0089eb9b17b6
John W <john.weaver94@utexas.edu>
2022-11-15 19:05:16 -0600

    I completed the question array, function for writing the README file and the function to intialize the app. I also finished the generateMarkdown. I should have pushed more!

--- index.js
+++ index.js
@@ -1,13 +1,93 @@
-// TODO: Include packages needed for this application
+const fs = require('fs');  
+const util = require('util');
+const fileWriting = util.promisify(fs.writeFile);
+const inquirer =require('inquirer')
+const generateMarkdown = require('./utils/generateMarkdown');
+// Array of questions for user input
+const questions = () => {
+    return inquirer.prompt([
+    {
+        type: 'input',
+        message: 'Enter the project title?',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'Enter a description of your project.',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'Enter installation instructions for your project.',
+        name: 'install'
+    },
+    {
+        type: 'input',
+        message: 'Enter usage instructions for your project.',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'Enter contribution guidelines for your project.',
+        name: 'contribution',
+    },
+    {
+        type: 'input',
+        message: 'Enter test cases for your project.',
+        name: 'test',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Select a license for your project',
+        choices: ['Apache License 2.0', 'BSD 3-Clause \'New\' or \'Revised\' license','Eclipse Public License version 2.0','Mozilla Public License 2.0',  
+          'BSD 2-Clause \'Simplified\' or \'FreeBSD\' license', 'Common Development and Distribution License','GNU General Public License (GPL)', 
+          'GNU Library or \'Lesser\' General Public License (LGPL)', 'MIT license'  ],
+    },
+    {
+        type: 'input',
+        message: 'Write your GitHub Username.',
+        name: 'Github',
+    }, 
+    {
+        type: 'input',
+        message: 'Write your email address.',
+        name: 'email',
+    },
+    {
+        type: 'input',
+        message: 'Write down any questions here',
+        name: 'questions'
+    }
+]);
+}
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// The function to write the README file. I used async because it allows for a promise which I found useful.
+const writeToFile = async (fileName, data) => {
+    try{
+        await fileWriting(fileName, data);
+        console.log('Your README.md has been written!');
+    } catch (err) {
+        console.log(err);
+        console.log('An error has occured. Please try again.');
+    }
+};
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
+// Function to initilize the app
+const init = async () => {
+    console.log('Welcome to the README.md generator!');
+        try {
+            //when user prompts are finished then return answers to answers array
+            const selection = await questions();
+            //call the function in generateMarkdoen.js file to create the markdown for the readme file
+            const markedDown = generateMarkdown.generateMarkdown(selection);
+            //call the writeToFile function and pass it the name 'README.MD' and the completed markdown
+            writeToFile('README.md', markedDown);
+        } catch (err) {
+            console.log(err);
+            console.log('There was an error with user input');
+        }
+};
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,80 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+//Array for liscenses for the badge icon
+const licensesLIst = [  
+  { name: 'Apache License 2.0', abbr: 'Apache', url: 'https://opensource.org/licenses/Apache-2.0'},
+  { name: 'BSD 3-Clause \'New\' or \'Revised\' license', abbr: 'BSD', url: 'https://opensource.org/licenses/BSD-3-Clause'},
+  { name: 'BSD 2-Clause \'Simplified\' or \'FreeBSD\' license', abbr: 'BSD', url: 'https://opensource.org/licenses/BSD-2-Clause'},
+  { name: 'GNU General Public License (GPL)', abbr: 'GNU', url: 'https://opensource.org/licenses/gpl-license'},
+  { name: 'GNU Library or \'Lesser\' General Public License (LGPL)', abbr: 'GNU', url: 'https://opensource.org/licenses/lgpl-license'},
+  { name: 'MIT license', abbr: 'MIT', url: 'https://opensource.org/licenses/MIT'},
+  { name: 'Mozilla Public License 2.0', abbr: 'Mozilla', url: 'https://opensource.org/licenses/MPL-2.0'},
+  { name: 'Common Development and Distribution License', abbr: 'CDDL', url: 'https://opensource.org/licenses/CDDL-1.0'},
+  { name: 'Eclipse Public License version 2.0', abbr: 'Eclipse', url: 'https://opensource.org/licenses/EPL-2.0'},
+];
 
-// TODO: Create a function that returns the license link
+//compare licenses and selects the one they picked!
+const fLiscense = (license) => {
+  for (userLiscense of licensesLIst) {
+    if (userLiscense.name === license)
+    return userLiscense;
+  }
+}
+// a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function createLiscense(license) {
+  let userLiscense = fLiscense(license);
+  return userLiscense ? `![license](https://img.shields.io/static/v1?label=license&message=${userLiscense.abbr}&color=brightgreen)` : '';
+}
 
-// TODO: Create a function that returns the license section of README
+// a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseLink(license) {
+  let userLiscense = fLiscense(license);
+  return userLiscense ? userLiscense.url : '';
+}
 
-// TODO: Create a function to generate markdown for README
+// a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  let userLiscense = fLiscense(license);
+  return userLiscense ? `Licensed under ${userLiscense.name}` : '';
+}
+
+// generateMarkdown function for README creation.
 function generateMarkdown(data) {
-  return `# ${data.title}
+  const licenseBadge = createLiscense(data.license);
+  const licenseLink = renderLicenseLink(data.license);
+  const licenseSection = renderLicenseSection(data.license);
 
+  return `# ${data.title}
+  ${licenseBadge}
+  ## Description
+  ${data.description}
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contribution](#contribution)
+  * [Tests](#test)
+  * [License](#license)
+  * [Questions](#questions)
+  * [Contacts](#contacts)
+  
+  ## Installation
+  ${data.install}
+  ## Usage
+  ${data.usage}
+  ## Contribution
+  ${data.contribution}
+  ## Tests
+  ${data.test}
+  ## License
+  ${licenseSection}
+  ${licenseLink}
+  ## Questions
+  ${data.questions}
+  ## Contacts
+  * GitHub: [${data.Github}](https://github.com/${data.Github})
+  * Email: [${data.email}](mailto:${data.email})
 `;
 }
 
-module.exports = generateMarkdown;
+module.exports.generateMarkdown = generateMarkdown;
